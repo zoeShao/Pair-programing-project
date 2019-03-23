99dd0ef8176c366b7a85acc328373e0b2f65cae6
linshiq4 <e.lin@mail.utoronto.ca>
2019-03-23 19:17:18 -0400

    Create Pizza and Drink classes Driver: linshiq4 Navigator: shaoyuti

---
+++ src/main/java/a2/Model/Pizza.java
@@ -0,0 +1,95 @@
+package a2.Model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Pizza {
+    private String size;
+    private String type;
+    private String recipe;  // specific method of preparation
+    private Map<String, Integer> toppingToQuantity;
+    public static Map<String, String> pizzaTypeToRecipe;
+    public static List<String> allToppings;
+
+    // Builder for Pizza
+    public static class PizzaBuilder {
+        private String size;
+        private String type;
+        private Map<String, Integer> toppingToQuantity = new HashMap<String, Integer>();
+
+        public PizzaBuilder setSize(String size){
+            if (!size.equals("Small") & !size.equals("Medium") & !size.equals("Large")) {
+                throw new IllegalArgumentException("size must be Small/Medium/Large. Please try again.");
+            }
+            this.size = size;
+            return this;
+        }
+
+        public PizzaBuilder setType(String type){
+            if (!Pizza.pizzaTypeToRecipe.keySet().contains(type)){
+                throw new IllegalArgumentException("Invalid type: " + type + ". Please try again.");
+            }
+            this.type = type;
+            return this;
+        }
+
+        public PizzaBuilder setToppings(Map<String, Integer> toppingToQuantity) {
+            this.toppingToQuantity = toppingToQuantity;
+            return this;
+        }
+
+        public PizzaBuilder updateToppings(String topping, Integer quantity){
+            if (!Pizza.allToppings.contains(topping)){
+                throw new IllegalArgumentException("Invalid topping: " + topping + ". Please try again.");
+            }
+            if (quantity < 0) {
+                throw new IllegalArgumentException("Quantity must be greater than 0. Please try again.");
+            }
+            if (quantity > 0) {
+                this.toppingToQuantity.put(topping, quantity);
+            } else {
+                this.toppingToQuantity.remove(topping);
+            }
+            return this;
+        }
+
+        public Pizza build(){
+            return new Pizza(this);
+        }
+    }
+
+    private Pizza(PizzaBuilder pizzaBuilder){
+        this.size = pizzaBuilder.size;
+        this.type = pizzaBuilder.type;
+        this.recipe = Pizza.pizzaTypeToRecipe.get(this.type);
+        this.toppingToQuantity = pizzaBuilder.toppingToQuantity;
+    }
+
+    public String getSize(){
+        return this.size;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getRecipe() {
+        return this.recipe;
+    }
+
+    public Map<String, Integer> getToppings(){
+        return this.toppingToQuantity;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder(this.size + " " + this.type + "Pizza with: ");
+        for (String topping: this.toppingToQuantity.keySet()) {
+            string.append(topping)
+                    .append("*")
+                    .append(this.toppingToQuantity.get(topping)).append("  ");
+        }
+        return string.toString();
+    }
+}
--- src/main/java/a2/PizzaParlour.java
+++ src/main/java/a2/PizzaParlour.java
@@ -1,15 +1,47 @@
 package a2;
-import java.util.Scanner;
+import a2.Model.Drink;
+import a2.Model.Pizza;
+
+import java.util.*;
 
 public class PizzaParlour {
 
     public static void main(String[] args) {
 
+        List allToppings = new ArrayList<String>();
+        allToppings.add("tomatoes");
+        allToppings.add("olives");
+        Pizza.allToppings = allToppings;
+        Map<String, String> allRecipes = new HashMap<String, String>();
+        allRecipes.put("Pepperoni", "Hi");
+        allRecipes.put("Margherita", "Oh");
+        Pizza.pizzaTypeToRecipe = allRecipes;
+        List drinks = new ArrayList<String>();
+        drinks.add("coke");
+        drinks.add("soda");
+        Drink.allDrinks = drinks;
+
         System.out.println("Welcome to 301 Pizza!: ");
         System.out.println("Would you like to order in, or make a delivery order?: ");
         Scanner scanner = new Scanner(System.in);
         String orderType = scanner.nextLine();
-
+        try {
+            Pizza.PizzaBuilder builder = new Pizza.PizzaBuilder();
+            builder.setSize("Small");
+            builder.setType("Pepperoni");
+            builder.updateToppings("tomatoes", 3);
+            builder.updateToppings("olives", 9);
+            builder.updateToppings("tomatoes", 0);
+            Pizza p = builder.build();
+            System.out.println(p.getRecipe());
+            System.out.println(p);
+            Drink d = new Drink("soda");
+            System.out.println(d);
+            Drink dk = new Drink("sod");
+            System.out.println(dk);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         scanner.close();
 
     }
