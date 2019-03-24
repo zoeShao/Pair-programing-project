fb3a0365075205846301f91dbfea20421aa7ca87
linshiq4 <e.lin@mail.utoronto.ca>
2019-03-24 01:54:45 -0400

    Create basic view for making order and viewing menu in main Driver: linshiq4 Navigator: shaoyuti

--- src/main/java/a2/PizzaParlour.java
+++ src/main/java/a2/PizzaParlour.java
@@ -17,29 +17,47 @@ public class PizzaParlour {
         Pizza.pizzaTypeToRecipe = ParlourFileReader.readPizzaRecipe(filePath + "Recipes.txt");
         Pizza.allToppings = Order.toppingToPrice.keySet();
 
+        String startMessage = "If you want to see the menu, please enter Menu.\n"
+                + "If you want to make an order, please enter Order.\n"
+                + "If you want to exit the program, please enter Exit.";
+
         System.out.println("Welcome to 301 Pizza!: ");
-        System.out.println("Would you like to order in, or make a delivery order?: ");
+        System.out.println(startMessage);
         Scanner scanner = new Scanner(System.in);
-        String orderType = scanner.nextLine();
-        try {
-            Pizza.PizzaBuilder builder = new Pizza.PizzaBuilder();
-            builder.setSize("Small");
-            builder.setType("Pepperoni");
-            builder.updateToppings("Tomatoes", 3);
-            builder.updateToppings("Olives", 9);
-            builder.updateToppings("Tomatoes", 0);
-            Pizza p = builder.build();
-            Order order = new Order();
-            order.addPizza(p);
-            order.updateDrink("Pepsi", 3);
-            order.updateDrink("Coke", 4);
-            order.calculateTotalPrice();
-            System.out.println(order);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        String option;
+
+        while (!(option = scanner.nextLine()).equals("Exit")) {
+            try {
+                if (option.equals("Menu")) {
+                    System.out.println("Menu");
+                    System.out.println(startMessage);
+                } else if (option.equals("Order")) {
+                    System.out.println("Order");
+                    Pizza.PizzaBuilder builder = new Pizza.PizzaBuilder();
+                    builder.setSize("Small");
+                    builder.setType("Pepperoni");
+                    builder.updateToppings("Tomatoes", 3);
+                    builder.updateToppings("Olives", 9);
+                    builder.updateToppings("Tomatoes", 0);
+                    Pizza p = builder.build();
+                    Order order = new Order();
+                    order.addPizza(p);
+                    order.updateDrink("Pepsi", 3);
+                    order.updateDrink("Coke", 4);
+                    order.calculateTotalPrice();
+                    System.out.println(order);
+//                    System.out.println("Would you like to pick up, or make a delivery order?: ");
+
+                } else {
+                    System.out.println("Invalid Input. Please enter Menu/Order/Exit.");
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         scanner.close();
-
+        System.exit(0);
     }
 
 }
