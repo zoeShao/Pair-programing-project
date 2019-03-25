8714be9005191da5cdeb64679873a13f49e5d2a1
linshiq4 <e.lin@mail.utoronto.ca>
2019-03-24 23:56:07 -0400

    Clean code Nor pair programming

--- src/test/java/a2/ControllerTest/HandlerTest.java
+++ src/test/java/a2/ControllerTest/HandlerTest.java
@@ -1,19 +1,4 @@
-//package a2.ControllerTest;
-//
-//import a2.Controller.Handler;
-//import org.junit.Test;
-//
-//import java.io.ByteArrayInputStream;
-//import java.io.InputStream;
-//import java.util.Scanner;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class HandlerTest {
-//    Handler handler= new Handler();
-//
-//    @Test
-//    public void test
-//
-//
-//}
+package a2.ControllerTest;
+
+public class HandlerTest {
+}
--- src/test/java/a2/ControllerTest/OrderUpdaterTest.java
+++ src/test/java/a2/ControllerTest/OrderUpdaterTest.java
@@ -1,18 +1,5 @@
-//package a2.ControllerTest;
-//
-//import a2.Controller.OrderUpdater;
-//import org.junit.Test;
-//
-//import java.io.ByteArrayInputStream;
-//import java.util.Scanner;
-//
-//public class OrderUpdaterTest {
-//    OrderUpdater orderUpdater = new OrderUpdater();
-//
-//    @Test
-//    public void testUpdatePizza(){
-//        String input = "Pizza\nAdd\nSmall\nPepperoni\nYes\nPepperoni:3\nNo\n";
-//        System.setIn(new ByteArrayInputStream(input.getBytes()));
-//        orderUpdater.setScanner(new Scanner(System.in));
-//    }
-//}
+package a2.ControllerTest;
+
+public class OrderUpdaterTest {
+
+}
